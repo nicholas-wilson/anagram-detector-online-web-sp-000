f1f1e5c9cb324b4ec4bdccfdf457e9811b2b574c
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-12-04 01:01:28 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,14 +5,14 @@ class Anagram
     main_word = word
     letters = word.split()
     letters.sort!
-    matching_words = ["a word"]
+    :matching_words = ["a word"]
   end
 
   def match(words)
     @words = words
     current_letters = []
     @words.each do |word|
-      puts "#{matching_words} and then #{@matching_words}"
+      puts "#{:matching_words} and then #{@matching_words}"
     end
   end
 end
