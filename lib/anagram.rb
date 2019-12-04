6a01202ac760580ebda659ee59cf7f3195b03034
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-12-04 00:54:13 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,4 +1,14 @@
 # Your code goes here!
 class Anagram
+  attr_accessor :main_word, :letters
+  def initialize(word)
+    main_word = word
+    letters = word.split()
+    letters.sort!
+  end
 
+  def match(words)
+    @words = words.split(" ")
+
+  end
 end
