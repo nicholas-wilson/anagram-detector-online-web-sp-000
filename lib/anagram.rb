f684fb5603b79978b83fef2748c400f1a42a453c
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-12-04 01:11:17 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,18 +3,16 @@ class Anagram
   attr_accessor :main_word, :letters, :matching_words
   def initialize(word)
     @main_word = word
-    @letters = word.split()
-    #@letters.sort!
-    puts "#{@main_word}, and then #{@letters}"
+    @letters = word.split("")
+    @letters.sort!
     @matching_words = []
   end
 
   def match(words)
     words.each do |word|
-      current_letters = word.split()
+      current_letters = word.split("")
       current_letters.sort!
       if current_letters == @letters
-        puts "Current: #{current_letters} , letters: #{@letters}"
         @matching_words << word
       end
     end
